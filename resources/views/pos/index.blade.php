82707014e5635c2f4ca5d382b04219f5de6b2b22
safullah <safullahzafar@gmail.com>
2026-05-20 16:27:38 +0500

    Initial Laravel commit

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -822,7 +822,7 @@
             <path
               d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
           </svg>
-          Client Identification
+          Client Identifications
         </div>
 
         <div style="display:grid; grid-template-columns:1fr 1fr; gap:8px; margin-bottom:10px;">
